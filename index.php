ce2e72708a810e4a2a75bad085e1c9314b45065f
Patrick Kamau <116178417+Patrick-K-N@users.noreply.github.com>
2023-05-02 13:02:38 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//database credentials
+$host = 'localhost';
+$username = 'your_username';
+$password = 'your_password';
+$dbname = 'your_database_name';
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
