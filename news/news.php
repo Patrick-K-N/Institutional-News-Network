cbf69b97fcd8d0069e5ef1e9cbd7fb5b0d20415a
Patrick Kamau <116178417+Patrick-K-N@users.noreply.github.com>
2023-05-04 21:25:48 +0300

    Add files via upload

---
+++ news/news.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chuka University Ranks Top in Eastern Africa</title>
+	<style>
+		body {
+			background-color: #87CEFA;
+			font-family: Arial, sans-serif;
+			color: #000000;
+		}
+
+		#news {
+			background-color: #ffffff;
+			padding: 20px;
+			box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+		}
+
+		#news h2 {
+			color: #003399;
+			font-size: 2rem;
+			margin-bottom: 20px;
+			text-shadow: 1px 1px 1px #ccc;
+		}
+
+		#news ul li a {
+			color: #003399;
+			text-decoration: none;
+			display: block;
+			padding: 20px;
+			border-bottom: 1px solid #eee;
+			transition: all 0.3s ease-in-out;
+			animation: fade-in 0.5s ease-in-out;
+		}
+
+		#news ul li a:hover {
+			background-color: #f7f7f7;
+			box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+		}
+
+		@keyframes fade-in {
+			from {
+				opacity: 0;
+				transform: translateY(-20px);
+			}
+			to {
+				opacity: 1;
+				transform: translateY(0);
+			}
+		}
+	</style>
+</head>
+<body>
+	<div id="news">
+		<h2>New Library opened officially!</h2>
+		<p>The opening of a new library building on a campus is a significant milestone in the life of an academic institution. The library is a crucial part of the academic community, as it provides access to essential resources, including books, journals, databases, and other materials that are critical for research and learning. A well-equipped library is essential for students, researchers, and faculty to conduct their research and academic work.
+
+The opening of a new library building is an exciting event that is eagerly anticipated by the entire academic community. The new building is a symbol of progress and development, and it represents the institution's commitment to academic excellence. The construction of a new library is a complex process that involves many stakeholders, including architects, engineers, builders, and the academic community. The design of the library building must be carefully planned and executed to ensure that it meets the needs of the users and provides a conducive learning environment.
+
+The new library building is typically equipped with state-of-the-art technology and infrastructure that provides users with access to the latest research tools and resources. The library is an essential part of the academic ecosystem, and it plays a critical role in supporting research and learning. A well-equipped library is critical for the success of the academic community, and it enables students and researchers to access a vast array of resources that are essential for their academic and professional growth.</p>	</div>
+	<title>Chuka University Ranks Top in Eastern Africa</title>
+	<style>
+		body {
+			background-color: #87CEFA;
+			font-family: Arial, sans-serif;
+			color: #000000;
+		}
+
+		#news {
+			background-color: #ffffff;
+			padding: 20px;
+			box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+		}
+
+		#news h2 {
+			color: #003399;
+			font-size: 2rem;
+			margin-bottom: 20px;
+			text-shadow: 1px 1px 1px #ccc;
+		}
+
+		#news ul li a {
+			color: #003399;
+			text-decoration: none;
+			display: block;
+			padding: 20px;
+			border-bottom: 1px solid #eee;
+			transition: all 0.3s ease-in-out;
+			animation: fade-in 0.5s ease-in-out;
+		}
+
+		#news ul li a:hover {
+			background-color: #f7f7f7;
+			box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+		}
+
+		@keyframes fade-in {
+			from {
+				opacity: 0;
+				transform: translateY(-20px);
+			}
+			to {
+				opacity: 1;
+				transform: translateY(0);
+			}
+		}
+	</style>
+</head>
+<body>
+	<div id="news">
+		<h2>Chuka University Ranks Top in Eastern Africa</h2>
+		<p>Chuka University in Kenya has been ranked among the top universities in Eastern Africa due to its commitment to quality education, research, and innovation. The university has established partnerships with international institutions and has invested in state-of-the-art facilities to enhance learning and research. Chuka also offers a wide range of programs in various fields, including agriculture, health sciences, and business, among others, which cater to the diverse needs of its students.</p>
+	</div>
+</body>
+<h3>Chuka University Launches New Security Feature</h3>
+<p> Chuka University is proud to announce the launch of its new security features aimed at ensuring the safety of its students and staff. In light of recent security concerns on campuses across the country, the Campus has taken proactive steps to enhance its security measures.</p>
+	</div>
+	<title>Chuka University Ranks Top in Eastern Africa</title>
+	<style>
+		body {
+			background-color: #87CEFA;
+			font-family: Arial, sans-serif;
+			color: #000000;
+		}
+
+		#news {
+			background-color: #ffffff;
+			padding: 20px;
+			box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+		}
+
+		#news h2 {
+			color: #003399;
+			font-size: 2rem;
+			margin-bottom: 20px;
+			text-shadow: 1px 1px 1px #ccc;
+		}
+
+		#news ul li a {
+			color: #003399;
+			text-decoration: none;
+			display: block;
+			padding: 20px;
+			border-bottom: 1px solid #eee;
+			transition: all 0.3s ease-in-out;
+			animation: fade-in 0.5s ease-in-out;
+		}
+
+		#news ul li a:hover {
+			background-color: #f7f7f7;
+			box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+		}
+
+		@keyframes fade-in {
+			from {
+				opacity: 0;
+				transform: translateY(-20px);
+			}
+			to {
+				opacity: 1;
+				transform: translateY(0);
+			}
+		}
+	</style>
+	</body>
+</html>
+
